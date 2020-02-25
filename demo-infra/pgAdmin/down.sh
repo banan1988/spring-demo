b334224bc33763d79a20eb9e0c77178a33b235af
Łukasz Kucharski <banan@b2d.com.pl>
2020-02-25 20:47:03 +0100

    [demo-infra] Added pgAdmin

---
+++ demo-infra/pgAdmin/down.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Let's remove things created by docker-compose up:"
+printf "\t- Containers for services defined in the Compose file\n"
+printf "\t- Networks defined in the 'networks' section of the Compose file\n"
+printf "\t- The default network, if one is used\n"
+
+docker-compose down
+EXIT_CODE=$?
+
+if [[ ${EXIT_CODE} != 0 ]]; then
+  exit 1
+fi
+
+echo "----------------------------------------"
+docker ps
+echo "----------------------------------------"
+docker volume ls
