774d10ce1f0b827b270e72a6c42e1c144eef5ffc
Łukasz Kucharski <banan@b2d.com.pl>
2020-02-26 10:46:35 +0100

    [demo-infra][nexus] Aligned configuration to docker swarm solution.

--- demo-infra/nexus/down.sh
+++ demo-infra/nexus/down.sh
@@ -5,7 +5,9 @@ printf "\t- Containers for services defined in the Compose file\n"
 printf "\t- Networks defined in the 'networks' section of the Compose file\n"
 printf "\t- The default network, if one is used\n"
 
-docker-compose down
+STACK="${1:-demo}"
+
+docker stack rm "${STACK}"
 EXIT_CODE=$?
 
 if [[ ${EXIT_CODE} != 0 ]]; then
@@ -13,6 +15,11 @@ if [[ ${EXIT_CODE} != 0 ]]; then
 fi
 
 echo "----------------------------------------"
-docker ps
+echo "Left running dockers:"
 echo "----------------------------------------"
-docker volume ls
+docker ps
+
+echo "----------------------------------------"
+echo "Left stack[${STACK}] volumes:"
+echo "----------------------------------------"
+docker volume ls | grep "VOLUME\|${STACK}"
--- demo-infra/nexus/up.sh
+++ demo-infra/nexus/up.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-docker-compose up -d
+STACK="${1:-demo}"
+
+docker stack deploy --compose-file docker-compose.yaml "${STACK}"
 EXIT_CODE=$?
 
 if [[ ${EXIT_CODE} != 0 ]]; then
     exit 1
 fi
 
+echo "----------------------------------------"
+echo "Running stacks:"
+echo "----------------------------------------"
+docker stack ls
+
+echo "----------------------------------------"
+echo "Running dockers in stack[${STACK}]:"
+echo "----------------------------------------"
+docker stack ps "${STACK}"
+
+echo "----------------------------------------"
+echo "Running dockers:"
+echo "----------------------------------------"
 docker ps
