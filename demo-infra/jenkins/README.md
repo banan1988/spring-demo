77573cf66f37c902c70d03d1659c73158952e3cd
Łukasz Kucharski <banan@b2d.com.pl>
2020-02-29 10:11:38 +0100

    [demo-infra] Aligned sonarqube to docker swarm. Set replicas: 0 for jenkins slave

---
+++ demo-infra/jenkins/README.md
@@ -0,0 +1,10 @@
+# How to pass env variables ?
+Populate env variables:
+```shell script
+export CREDENTIALS_GITHUB_USERNAME=myUsername
+export CREDENTIALS_GITHUB_PASSWORD=myPassword
+```
+Deploy stack:
+```shell script
+./up.sh
+```
--- demo-infra/sonarqube/up.sh
+++ demo-infra/sonarqube/up.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-docker-compose up -d
+STACK="${1:-demo-sonarqube}"
+
+docker stack deploy --compose-file docker-compose.yaml "${STACK}"
 EXIT_CODE=$?
 
 if [[ ${EXIT_CODE} != 0 ]]; then
-    exit 1
+  exit 1
 fi
 
-docker ps
+sleep 20
+
+echo "----------------------------------------"
+echo "Running stacks:"
+echo "----------------------------------------"
+docker stack ls
+
+echo "----------------------------------------"
+echo "Running dockers in stack[${STACK}]:"
+echo "----------------------------------------"
+docker stack ps "${STACK}"
+
+echo "----------------------------------------"
+echo "Running dockers:"
+echo "----------------------------------------"
+docker ps | grep "CONTAINER\|${STACK}"
